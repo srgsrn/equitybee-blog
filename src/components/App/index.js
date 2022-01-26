99b0676f08c65c739b13150bc470c52da91449cb
srgsrn <srnsergey@gmail.com>
2022-01-26 13:34:35 +0200

    feat: update react-scripts to v5, yarn upgrade all

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className='container'>
+    <div className='app container mx-auto'>
       <h1 className='text-3xl font-bold underline'>Hello there!</h1>
     </div>
   );
