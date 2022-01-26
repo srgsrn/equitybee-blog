ed8a491ead66a05d1a7ec7cdbc7130603d3843e4
srgsrn <srnsergey@gmail.com>
2022-01-26 13:05:51 +0200

    feat: add tailwind css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello there!</h1>
+    <div className='container'>
+      <h1 className='text-3xl font-bold underline'>Hello there!</h1>
     </div>
   );
 }
